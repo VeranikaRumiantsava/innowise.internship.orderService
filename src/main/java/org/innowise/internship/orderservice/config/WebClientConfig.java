5cb7a3dea15175e59bca84046e9ddd2a012ec56b
rumronya <rum.ronya@gmail.com>
2025-08-25 13:44:26 +0300

    it

--- src/main/java/org/innowise/internship/orderservice/config/WebClientConfig.java
+++ src/main/java/org/innowise/internship/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package org.innowise.internship.orderservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,9 +9,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient() {
+    public WebClient webClient(@Value("${user.service.url}") String baseUrl) {
         return WebClient.builder()
-                .baseUrl("http://userservice:8080")
+                .baseUrl(baseUrl)
                 .build();
     }
 }
